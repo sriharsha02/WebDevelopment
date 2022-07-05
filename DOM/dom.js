315b9a7a5cb3309e7ad2a9073d1e90d24e06f489
sriharsha02 <akshintalasriharsha@yahoo.com>
2022-07-06 00:33:17 +0530

    used getElementsByClass

--- DOM/dom.js
+++ DOM/dom.js
@@ -19,3 +19,15 @@ header.style.borderBottom = 'solid 3px #000'
 var addItem = document.getElementById('add-items');
 addItem.style.fontWeight = 'bold';
 addItem.style.color = 'green';
+
+//get elements by class name
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[1].textContent = 'Hello';
+items[2].style.backgroundColor = 'green';
+items[0].style.fontWeight = 'bold';
+items[1].style.fontWeight = 'bold';
+items[2].style.fontWeight = 'bold';
+items[3].style.fontWeight = 'bold';
+
