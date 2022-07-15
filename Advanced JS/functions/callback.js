63f4141f483ccd876ec61e51d9ce8aa2941a0650
sriharsha02 <akshintalasriharsha@yahoo.com>
2022-07-15 19:05:00 +0530

    modified callback program

--- Advanced JS/functions/callback.js
+++ Advanced JS/functions/callback.js
@@ -2,14 +2,14 @@
 // settimeout will take the callback function and it wil
 //store the function in separate space with a timer of 5000ms
 //JS will not wait and it will move on to next steps
-setTimeout(function(){ 
-  console.log('timer');
-}, 5000)
+setTimeout(() => console.log('timer1 expired'), 1000);
+setTimeout(()=> console.log('timer2 expired'), 0);
+
 function x(y){
   console.log('inside x');
   y();
 
 }
 x(function y(){
-  console.log('inside y');
+  setTimeout(() => console.log('inside y'),0);
 });
