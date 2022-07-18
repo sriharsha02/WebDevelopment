80f5e296fd18de791e8369166a7c5d4bc9bbd735
sriharsha02 <akshintalasriharsha@yahoo.com>
2022-07-18 12:52:29 +0530

    promises introduction

---
+++ Async In JavaScript/promises.js
@@ -0,0 +1,55 @@
+const posts = [
+  {title : 'Post One', body: 'This is post one'},
+  {title : 'Post Two', body: 'This is post two'}
+];
+function getPosts(){
+  setTimeout(() => {
+    let output = '';
+    for(let i = 0; i < posts.length; i++) {
+      output += `<li>${posts[i].title}</li>`
+    }
+    document.body.innerHTML = output;
+  }, 1000);
+}
+function createPost(post){
+  return new Promise((resolve, reject) =>{
+   setTimeout(() => {
+     posts.push(post);
+     const error = false;
+     if(!error){
+       resolve();
+     }
+     else{
+       reject('Error: Something went wrong');
+     }
+   }, 2000);
+   });
+}
+
+function deletePost(){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(posts.values !== 0){
+        resolve(posts.pop());
+      }
+      else{
+        reject('Array is empty now');
+      }
+    }, 1000);
+  });
+}
+
+createPost({title : 'Post Three', body: 'This is post three'})
+.then(() => {
+  getPosts();
+  deletePost().then(() => {
+    getPosts();
+  }).catch(err => console.log(err))
+  deletePost().then(() => {
+    getPosts();
+  }).catch(err => console.log(err))
+}).catch(err => console.log(err))
+
+
+
+
