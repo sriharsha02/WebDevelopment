737b5fd172d0e8f812628cbe7594dd95071bdf83
sriharsha02 <akshintalasriharsha@yahoo.com>
2022-08-11 05:15:15 +0530

    added request routing

--- NodeJS/ReqRes/app.js
+++ NodeJS/ReqRes/app.js
@@ -1,14 +1,20 @@
 const http = require('http');
 
 const express = require('express');
-
+const bodyParser = require('body-parser')
 const app = express();
-app.use((req, res, next) => {
-  console.log('In the Middleware');
-  next();
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><br><input type="text" name="size"><br><button type="submit">Add Product</button></form>')
+})
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 })
 app.use((req, res, next) => {
-  console.log('In another Middleware');
   res.send('<h1>Hello World</h1>');
 })
 
