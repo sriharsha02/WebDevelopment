c89dd7f7daa02f3715e74fe396b74f94c93d65bf
sriharsha02 <akshintalasriharsha@yahoo.com>
2022-08-26 20:26:03 +0530

    added CRUD operations with sequelize

--- NodeJS/DynamicRoutes1/cart model/app.js
+++ NodeJS/DynamicRoutes1/cart model/app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const db = require('./util/database')
+const sequelize = require('./util/database')
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -25,4 +25,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
