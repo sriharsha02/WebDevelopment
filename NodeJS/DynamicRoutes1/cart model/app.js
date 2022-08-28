919d495ecded15441449de25ee7e41e4ca600ff8
sriharsha02 <akshintalasriharsha@yahoo.com>
2022-08-28 17:54:08 +0530

    used associations in sequelize

--- NodeJS/DynamicRoutes1/cart model/app.js
+++ NodeJS/DynamicRoutes1/cart model/app.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const sequelize = require('./util/database')
+const Product = require('./models/product')
+const User = require('./models/user')
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -20,14 +22,37 @@ const { deleteById } = require('./models/product');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
+User.hasMany(Product);
+
+sequelize.sync({})
   .then((result) => {
+    return User.findByPk(1)
     // console.log(result);
+  })
+  .then(user => {
+    if(!user){
+      User.create({name: 'harsha', email: '123@gmail.com'})
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch(err => {
