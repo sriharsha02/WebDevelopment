5cbe6f7acd43b526c233e5187b71d26be898ea78
sriharsha02 <akshintalasriharsha@yahoo.com>
2022-09-08 17:12:22 +0530

    added a cart popup menu

--- Projects/Ecommerce/website/script.js
+++ Projects/Ecommerce/website/script.js
@@ -1,10 +1,14 @@
-const btn = document.getElementsByClassName("add-to-cart")
-const container = document.getElementById('notification-div')
+const btn = document.getElementsByClassName("add-to-cart");
+const container = document.getElementById('notification-div');
+const open = document.getElementById('main-cart-button');
+const close = document.getElementById('close');
+const cartContainer = document.getElementById('cart-container');
 
 for(const button of btn)
   button.addEventListener('click', () => {
     createNotification()
   })
+
 function createNotification(){
   const notif = document.createElement('div')
   notif.classList.add('toast')
@@ -16,11 +20,14 @@ function createNotification(){
     notif.remove()
   }, 3000)
 }
+open.addEventListener('click', () =>{
+  cartContainer.classList.add('active')
+  
+})
+close.addEventListener('click', () =>{
+  cartContainer.classList.remove('active')
+  
+})
 
 
-
-// function addToCart(event){
-//   const buttonID =  event.target.id;
-//   console.log(buttonID)
-// }
 
